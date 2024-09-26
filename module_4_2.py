6b196857f61ced63c6e9d57ebe1d5674cb263580
MichailBolsun32 <bolsun32@mail.ru>
2024-09-26 20:28:53 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -2,10 +2,13 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
 
-    return inner_function
+    inner_function()
 
 
-inner_function = test_function()
-inner_function()
+test_function()
 
-
+# Пробую вызывать inner_function вне test_function
+try:
+    inner_function()
+except NameError as error:
+    print(f"Ошибка: {error}")
